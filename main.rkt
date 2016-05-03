b82e2cc5f72bb06f33179f9b10ab3ecb0ca762c4
unicodesnowman <utfsnowman@gmail.com>
2016-05-03 12:58:46 -0400

    finish ch 3

--- main.rkt
+++ main.rkt
@@ -68,3 +68,21 @@
        (multirember a (cdr lat)))
       (else (cons (car lat)
                   (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) old)
+       (cons old (cons new (multiinsertR new old (cdr lat)))))
+      (else (cons (car lat)
+                  (multiinsertR new old (cdr lat)))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) old)
+       (cons new (multisubst new old (cdr lat))))
+      (else (cons (car lat)
+                  (multisubst new old (cdr lat)))))))
