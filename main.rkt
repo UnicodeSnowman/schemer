cb440de8e1987a87299adb94a6aefde3548a7ef7
unicodesnowman <utfsnowman@gmail.com>
2016-05-03 15:57:17 -0400

    through =

--- main.rkt
+++ main.rkt
@@ -86,3 +86,68 @@
        (cons new (multisubst new old (cdr lat))))
       (else (cons (car lat)
                   (multisubst new old (cdr lat)))))))
+
+; take a and add 1 to it b times
+(define +
+  (lambda (a b)
+    (cond
+      ((zero? b) a)
+      (else (add1 (+ a (sub1 b)))))))
+
+; take b and subtract 1 from it b times
+(define -
+  (lambda (a b)
+    (cond
+      ((zero? b) a)
+      (else (sub1 (- a (sub1 b)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (+ (car tup)
+               (addtup (cdr tup)))))))
+
+(define x
+  (lambda (a b)
+    (cond
+      ((zero? b) 0)
+      (else (+ a (x a (sub1 b)))))))
+
+; if tup1 is null on any iteration, just append tup2 to the existing list
+; if tup2 is null...
+; tup+ must always return a list, so this makes sense
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons
+             (+ (car tup1) (car tup2))
+             (tup+ (cdr tup1) (cdr tup2)))))))
+
+; ensuring that (zero? a) case appears first, we can assert that invocations where (eq? a b) returns #t
+; will result in a #f return value, since any time a reaches 0, we know we have a falsy end condition
+(define >
+  (lambda (a b)
+    (cond
+      ((zero? a) #f)
+      ((zero? b) #t)
+      (else (> (sub1 a) (sub1 b))))))
+
+(define <
+  (lambda (a b)
+    (cond
+      ((zero? b) #f)
+      ((zero? a) #t)
+      (else (< (sub1 a) (sub1 b))))))
+
+; longer form version of this also an option: check if both a and b are 0, then check if a is 0,
+; otherwise recur, subtracting 1 from a and b
+; = used for testing number equality, eq? for others
+(define =
+  (lambda (a b)
+    (cond
+      ((> a b) #f)
+      ((< a b) #f)
+      (else #t))))
